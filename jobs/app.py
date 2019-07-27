146e4763abd73b038a01b66fd4ee397bc92d8007
Myra Cui <melissa_cui@brown.edu>
2019-07-27 12:55:14 -0400

    starting over 2

--- jobs/app.py
+++ jobs/app.py
@@ -1,16 +1,16 @@
-# import sqlite3
-# from flask import Flask, render_template, g
+import sqlite3
+from flask import Flask, render_template, g
 
-# PATH = 'db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# def open_connection():
-#     connection = getattr(g, 'connection', None)
-#     if connection == None:
-#         connection = g._connection = sqlite3.connect(PATH)
-#     connection.row_factory = sqlite3.Row
-#     return connection
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 # def execute_sql(sql, values=(), commit = False, single = False):
 #     connection = open_connection()
@@ -29,8 +29,8 @@
 #     if connection is not None:
 #         connection.close()
 
-# @app.route('/')
-# @app.route('/jobs')
-# def jobs():
-#     jobs = execute_sql('SELECT job.id, q job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-#     return render_template('index.html', jobs=jobs)
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    #jobs = execute_sql('SELECT job.id, q job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html')#, jobs=jobs)
