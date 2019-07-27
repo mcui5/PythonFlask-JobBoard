e7aa1a7b0960660c27a3058cc8513182b671e4bf
Myra Cui <melissa_cui@brown.edu>
2019-07-27 14:09:31 -0400

    skrt skrt 3

--- jobs/app.py
+++ jobs/app.py
@@ -3,12 +3,18 @@ from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
+# __name__ is the name of the current class, function, method, 
+# descriptor, or generator instance
 app = Flask(__name__)
 
 def open_connection():
+    # gets attributes from imported g 
+    # g object makes things global over flask app
     connection = getattr(g, '_connection', None)
+    # checking if there is a connection
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
+    # converts the plain tuple into a more useful object
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -18,7 +24,8 @@ def execute_sql(sql, values=(), commit = False, single = False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone() if single else cursor.fetchall()
+        # fetches next row from active dataset, but if it's single, it will fetch all the rows
+        results = cursor.fetchone() if single else cursor.fetchall()   
 
     cursor.close()
     return results
@@ -29,8 +36,11 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
+# binding a function to a URL
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    #jobs = execute_sql('SELECT job.id, q job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    return render_template('index.html')#, jobs=jobs)
+    jobs = execute_sql('''SELECT job.id, job.title, job.description,
+    job.salary, employer.id as employer_id, employer.name as employer_name 
+    FROM job JOIN employer ON employer.id = job.employer_id''')
+    return render_template('index.html', jobs=jobs)
