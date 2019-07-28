df4ac2de3b24e5a5faf58269d681dac559f04b38
Myra Cui <melissa_cui@brown.edu>
2019-07-28 19:15:49 -0400

    bongg

--- jobs/app.py
+++ jobs/app.py
@@ -43,7 +43,14 @@ def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
 
+# binding function to URL
 @app.route('/job/<job_id>')
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
+
+@app.route('/employer/<employer_id>')
+def employer(employer_id):
+    employer = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
